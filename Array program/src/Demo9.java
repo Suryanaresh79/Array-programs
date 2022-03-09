2a134cc031ca08bb6fb6cda7234bc7e14e57b258
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-03-09 12:48:06 +0530

    Single dimensional array program

---
+++ Array program/src/Demo9.java
@@ -0,0 +1,12 @@
+//Demo on arrays
+public class Demo9 {
+	public static void main(String[] args) {
+	int a[] = {10,20,30,40,50};
+	int i;
+	for(i=0; i<5; i++)
+	{
+	System.out.println(a[i]);
+	}
+  }
+}
+
